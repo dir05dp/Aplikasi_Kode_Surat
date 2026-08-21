9df88adb1d42ed07d7a48afab953076581f9990c
Muhammad Takdir <130807205+dir05dp@users.noreply.github.com>
2026-08-21 21:16:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,8 @@ user_query = st.text_input("Contoh pencarian: pengadaan semen, cuti sakit, rapat
 
 if user_query:
     with st.spinner("AI sedang memindai ratusan halaman secara instan untuk Anda..."):
-        prompt = f"""Kamu adalah asisten arsiparis profesional di Kementerian PUPR. 
-        Gunakan SELURUH TEKS dokumen Permen PUPR No 16/PRT/M/2018 di bawah ini untuk mencari kode klasifikasi arsip yang ditanyakan.
-        Sebutkan kodenya, nama klasifikasi, dan alasan singkatnya. Jika tidak ada yang sama persis, berikan saran yang paling mendekati konsepnya.
+        prompt = f"""temukan kode klarifikasi arsip didalam dokumen Permen PUPR No 16/PRT/M/2018 itu yang berhubungan dengan yang ditanyakan.
+        Sebutkan kodenya, nama klasifikasi. Jika tidak ada yang sama persis, berikan kode klarifikasi yang paling mendekati dengan yang ditanyakan.
 
         TEKS DOKUMEN:
         {teks_pdf}
